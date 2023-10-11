f8607aaec1574501b31e4319fd14ec34312a2f3c
Tianjian <tianjianqin@outlook.com>
2023-10-11 15:05:07 +0200

    bash script for quantitative simulation

---
+++ Bash/run_quantitative_sim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=gnn_sim_qt_start
+#SBATCH --output=logs/gnn_sim_qt_start-%j.log
+#SBATCH --mem=1GB
+#SBATCH --partition=short
+
+ml R
+
+Rscript -e "devtools::install_github('EvoLandEco/eve')"
+
+name=${1}
+nrep=${2}
+
+for (( param_set = 1; param_set <= 405; param_set++ ))
+do
+sbatch ~/eve/Bash/submit_quantitative_sim.sh ${name} \
+                                    ${param_set} \
+                                    ${nrep}
+done
---
+++ Bash/submit_quantitative_sim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=3-04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=gnn_sim_qt
+#SBATCH --output=logs/gnn_sim_qt-%j.log
+#SBATCH --mem=8GB
+#SBATCH --partition=regular
+
+name=${1}
+param_set=${2}
+nrep=${3}
+
+ml R
+Rscript ~/eve/Script/quantitative_data.R ${name} \
+                                   ${param_set} \
+                                   ${nrep}
