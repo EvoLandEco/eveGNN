cec9068f6fc6d91189aa4abeb5d9e9d88580db32
Tianjian <tianjianqin@outlook.com>
2023-10-15 19:29:43 +0200

    test python script

--- Script/test.py
+++ Script/test.py
@@ -14,7 +14,17 @@ def get_params(name, set_index):
     set_index = int(set_index) - 1  # subtract 1 because iloc is 0-based
 
     # Print the specified row
-    print(df.iloc[set_index])
+    return df.iloc[set_index]
+
+
+def check_params(params):
+    # Check if all the list elements have the same lambda, mu, beta_n, and beta_phi
+    first_row = params[0]
+    for row in params[1:]:
+        if not (first_row[['lambda', 'mu', 'beta_n', 'beta_phi']].equals(row[['lambda', 'mu', 'beta_n', 'beta_phi']])):
+            print("Parameter mismatch found. Data is not consistent.")
+            return
+    print("All rows have the same lambda, mu, beta_n, and beta_phi.")
 
 
 def main():
@@ -26,13 +36,19 @@ def main():
     # The set_i folder names are passed as the remaining arguments
     set_paths = sys.argv[2:]
 
+    params_list = []
+
+    # Iterate through each set_i folder name
     for set_index in set_paths:
         set_path = f'set_{set_index}'
         # Concatenate the base directory path with the set_i folder name
         full_dir = os.path.join(name, set_path)
         # Call read_rds_to_pytorch with the full directory path
         print(full_dir)  # The set_i folder names are passed as the remaining arguments
-        get_params(name, set_index)
+        params_list.append(get_params(name, set_index))
+
+    # Check if all the list elements have the same lambda, mu, beta_n, and beta_phi
+    check_params(params_list)
 
 
 if __name__ == '__main__':
