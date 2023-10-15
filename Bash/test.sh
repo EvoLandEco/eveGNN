2d648a629afb948c1b6531a191554ec002459701
Tianjian <tianjianqin@outlook.com>
2023-10-15 11:37:58 +0200

    update script

--- Bash/test.sh
+++ Bash/test.sh
@@ -1,53 +1,33 @@
 #!/bin/bash
 
-# Get the name argument
-name=$1
-
-# Check if the name argument is provided
-if [[ -z $name ]]; then
-    echo "Usage: $0 <name>"
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_directory>"
     exit 1
 fi
 
-# File name
-file="${name}/params.txt"
+# Get the directory path argument
+path=$1
 
-# Check if the file exists
-if [[ ! -f $file ]]; then
-    echo "Error: File $file not found."
+# Check if the directory exists
+if [ ! -d "$path" ]; then
+    echo "Error: Directory $path not found."
     exit 1
 fi
 
-# Temporary file to hold intermediate results
-temp_file=$(mktemp)
+# Check if params.txt exists in the directory
+if [ ! -f "$path/params.txt" ]; then
+    echo "Error: params.txt not found in $path."
+    exit 1
+fi
 
-# Process the data file to get unique groups and their row indices
-awk '
-  NR > 1 {
-    # Create a group identifier
-    group = $5" "$6" "$7" "$8
+# Call the Python script
+python process_params.py "$path"
 
-    # Collect row indices per group
-    groups[group] = groups[group] ? groups[group]","NR : NR
-  }
-  END {
-    # Print the result
-    for (group in groups)
-      print group" "groups[group]
-  }
-' $file > $temp_file
+# Check if the Python script executed successfully
+if [ "$?" -ne 0 ]; then
+    echo "Error: Failed to execute Python script."
+    exit 1
+fi
 
-# Sort the result based on the group identifier
-sort $temp_file | awk '
-  {
-    # Split the indices into an array
-    split($NF, indices, ",")
-
-    # Print the indices on separate lines
-    for (i in indices)
-      print indices[i]
-  }
-' > grouped_indices.txt
-
-# Remove the temporary file
-rm $temp_file
+echo "Script executed successfully, output written to $path/output.txt"
