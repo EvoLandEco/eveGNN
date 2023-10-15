ee535331aedee42d4a353949034efb91e87003af
Tianjian <tianjianqin@outlook.com>
2023-10-15 19:15:52 +0200

    test python script

--- Script/test.py
+++ Script/test.py
@@ -1,5 +1,20 @@
 import sys
 import os
+import pandas as pd
+
+
+def get_params(name, set_index):
+    # Form the filename by concatenating the base directory with 'params.txt'
+    filename = os.path.join(name, 'params.txt')
+
+    # Read the table from the text file
+    df = pd.read_csv(filename, delim_whitespace=True, header=None)
+
+    # Convert set_index to integer
+    set_index = int(set_index) - 1  # subtract 1 because iloc is 0-based
+
+    # Print the specified row
+    print(df.iloc[set_index])
 
 
 def main():
@@ -17,6 +32,7 @@ def main():
         full_dir = os.path.join(name, set_path)
         # Call read_rds_to_pytorch with the full directory path
         print(full_dir)  # The set_i folder names are passed as the remaining arguments
+        get_params(name, set_index)
 
 
 if __name__ == '__main__':
