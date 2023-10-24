aa0854a8acd3db60d0bf5275a8063d125b34c259
Tianjian <tianjianqin@outlook.com>
2023-10-24 10:46:09 +0200

    disable MLE

--- Script/ddd_pars_est_cap_data.R
+++ Script/ddd_pars_est_cap_data.R
@@ -12,7 +12,7 @@ setwd("DDD_CAP_TES")
 
 cap_range <- c(10,1000)
 
-ddd_cap_tes_list <- replicate(5000, eveGNN::randomized_ddd_fixed_la_mu_age(cap_range = cap_range,
+ddd_cap_tes_list <- replicate(15000, eveGNN::randomized_ddd_fixed_la_mu_age(cap_range = cap_range,
                                                                      la = 0.6,
                                                                      mu = 0.1,
                                                                      age = 10,
@@ -22,7 +22,7 @@ ddd_cap_tes_list <- purrr::transpose(ddd_cap_tes_list)
 
 eveGNN::export_to_gnn_with_params(ddd_cap_tes_list, "tes")
 
-ddd_cap_tes_list_test <- eveGNN::get_test_data(ddd_cap_tes_list, 0.1)
+#ddd_cap_tes_list_test <- eveGNN::get_test_data(ddd_cap_tes_list, 0.1)
 
-mean_diffs <- eveGNN::compute_accuracy_dd_ml_fix_lamu(cap_range, ddd_cap_tes_list_test, strategy = "multicore", workers = 12)
-saveRDS(mean_diffs, "mean_diffs_DDD_CAP_TES.rds")
+#mean_diffs <- eveGNN::compute_accuracy_dd_ml_fix_lamu(cap_range, ddd_cap_tes_list_test, strategy = "multicore", workers = 12)
+#saveRDS(mean_diffs, "mean_diffs_DDD_CAP_TES.rds")
--- Script/ddd_pars_est_lamu_data.R
+++ Script/ddd_pars_est_lamu_data.R
@@ -15,12 +15,10 @@ dists <- list(
   list(distribution = "uniform", n = 1, min = 0, max = 0.4)
 )
 
-ddd_lamu_tes_list <- eveGNN::batch_sim_ddd(dists, 100, 10, 1, 5000)
+ddd_lamu_tes_list <- eveGNN::batch_sim_ddd(dists, 100, 10, 1, 15000)
 
 eveGNN::export_to_gnn_with_params(ddd_lamu_tes_list, "tes")
 
-ddd_lamu_tes_list_test <- eveGNN::get_test_data(ddd_lamu_tes_list, 0.1)
-
-mean_diffs <- eveGNN::compute_accuracy_dd_ml(dists, ddd_lamu_tes_list_test, strategy = "multicore", workers = 12)
-
-saveRDS(mean_diffs, "mean_diffs_DDD_LAMU_TES.rds")
+#ddd_lamu_tes_list_test <- eveGNN::get_test_data(ddd_lamu_tes_list, 0.1)
+#mean_diffs <- eveGNN::compute_accuracy_dd_ml(dists, ddd_lamu_tes_list_test, strategy = "multicore", workers = 12)
+#saveRDS(mean_diffs, "mean_diffs_DDD_LAMU_TES.rds")
