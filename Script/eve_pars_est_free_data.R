a6330b91f6f85084ee0229be4d4d17fa4bac1ab8
Tianjian <tianjianqin@outlook.com>
2023-10-31 10:55:41 +0100

    reduce scale of eve free data

--- Script/eve_pars_est_free_data.R
+++ Script/eve_pars_est_free_data.R
@@ -11,25 +11,25 @@ dir.create("EVE_FREE_TES")
 setwd("EVE_FREE_TES")
 
 dists <- list(
-  list(distribution = "uniform", n = 1, min = 0.5, max = 1.0),
-  list(distribution = "uniform", n = 1, min = 0, max = 0.4),
+  list(distribution = "uniform", n = 1, min = 0.5, max = 0.8),
+  list(distribution = "uniform", n = 1, min = 0, max = 0.3),
   list(distribution = "uniform", n = 1, min = -0.02, max = 0),
   list(distribution = "uniform", n = 1, min = -0.002, max = 0)
 )
 
-eve_free_tes_pd_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 10,
+eve_free_tes_pd_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 8,
                                                                 model = "dsce2",
                                                                 metric = "pd", offset = "simtime"), simplify = FALSE)
 eve_free_tes_pd_list <- purrr::transpose(eve_free_tes_pd_list)
 eveGNN::export_to_gnn_with_params_eve(eve_free_tes_pd_list, "tes")
 
-eve_free_tes_ed_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 10,
+eve_free_tes_ed_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 8,
                                                                 model = "dsce2",
                                                                 metric = "ed", offset = "none"), simplify = FALSE)
 eve_free_tes_ed_list <- purrr::transpose(eve_free_tes_ed_list)
 eveGNN::export_to_gnn_with_params_eve(eve_free_tes_ed_list, "tes")
 
-eve_free_tes_nnd_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 10,
+eve_free_tes_nnd_list <- replicate(5000, eveGNN::randomized_eve_fixed_age(dists, age = 8,
                                                                  model = "dsce2",
                                                                  metric = "nnd", offset = "none"), simplify = FALSE)
 eve_free_tes_nnd_list <- purrr::transpose(eve_free_tes_nnd_list)
