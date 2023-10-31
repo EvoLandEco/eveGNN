ac3815a7f185b4bcf82b904b845054994b77860a
Tianjian <tianjianqin@outlook.com>
2023-10-31 10:01:52 +0100

    script for eve pars est model training

---
+++ Bash/submit_eve_pars_est_model_training_diffpool.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1
+#SBATCH --job-name=gnn_eve_pars_est_diffpool
+#SBATCH --output=logs/gnn_eve_pars_est_diffpool-%j.log
+#SBATCH --mem=32GB
+#SBATCH --partition=gpu
+
+ml Python/3.8.16-GCCcore-11.2.0
+source $HOME/venvs/eve/bin/activate
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <name> <task_type>"
+    exit 1
+fi
+
+# Get the arguments
+name=$1
+task_type=$2
+
+# Call the Python script with the arguments
+python ../Script/train_eve_pars_est_DiffPool.py "$name" "$task_type"
+
+deactivate
