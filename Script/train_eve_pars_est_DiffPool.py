d7424a635638aa324790dc7c9fd74f26df20c23b
Tianjian <tianjianqin@outlook.com>
2023-11-01 15:22:46 +0100

    solve segmentation fault error

--- Script/train_eve_pars_est_DiffPool.py
+++ Script/train_eve_pars_est_DiffPool.py
@@ -610,8 +610,8 @@ def main():
             return False, inconsistent_cols
         return True, []
 
-    model_performance = pd.DataFrame()
-    final_differences = pd.DataFrame()
+    #model_performance = pd.DataFrame()
+    #final_differences = pd.DataFrame()
 
     # Convert the dictionary to a pandas DataFrame
     try:
