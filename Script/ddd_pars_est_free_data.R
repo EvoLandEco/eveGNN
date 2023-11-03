67865ff7e832664913737ebe05dba97b17d29096
Tianjian <tianjianqin@outlook.com>
2023-11-03 12:32:41 +0100

    another ddd pars free estimation

--- Script/ddd_pars_est_free_data.R
+++ Script/ddd_pars_est_free_data.R
@@ -17,7 +17,7 @@ dists <- list(
 
 cap_range <- c(10,1000)
 
-ddd_free_tes_list <- replicate(20000, eveGNN::randomized_ddd_fixed_age(dists,
+ddd_free_tes_list <- replicate(200, eveGNN::randomized_ddd_fixed_age(dists,
                                                                           cap_range = cap_range,
                                                                           age = 10,
                                                                           model = 1), simplify = FALSE)
@@ -26,6 +26,6 @@ ddd_free_tes_list <- purrr::transpose(ddd_free_tes_list)
 
 eveGNN::export_to_gnn_with_params(ddd_free_tes_list, "tes")
 
-ddd_free_tes_list_test <- eveGNN::get_test_data(ddd_free_tes_list, 0.025)
+ddd_free_tes_list_test <- eveGNN::get_test_data(ddd_free_tes_list, 0.5)
 mean_diffs <- eveGNN::compute_accuracy_dd_ml_free(dists, cap_range, ddd_free_tes_list_test, strategy = "multicore", workers = 16)
 saveRDS(mean_diffs, "mean_diffs_DDD_FREE_TES.rds")
