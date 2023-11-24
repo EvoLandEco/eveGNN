8829ca0617aae676059b679ca9771b520862037d
Tianjian <tianjianqin@outlook.com>
2023-11-24 16:00:42 +0100

    generate data completely unseen by trained models

--- Script/ddd_pars_est_free_data_fool.R
+++ Script/ddd_pars_est_free_data_fool.R
@@ -16,22 +16,22 @@ dir.create("DDD_FREE_TES")
 setwd("DDD_FREE_TES")
 
 dists <- list(
-  list(distribution = "uniform", n = 1, min = 0.5, max = 1.0),
-  list(distribution = "uniform", n = 1, min = 0, max = 0.4)
+  list(distribution = "uniform", n = 1, min = 1.0, max = 1.5),
+  list(distribution = "uniform", n = 1, min = 0.4, max = 0.8)
 )
 
 cap_range <- c(10,1000)
 
-ddd_free_tes_list <- replicate(50000, eveGNN::randomized_ddd_fixed_age(dists,
-                                                                          cap_range = cap_range,
-                                                                          age = 10,
-                                                                          model = 1), simplify = FALSE)
+ddd_free_tes_list <- replicate(10000, eveGNN::randomized_ddd_fixed_age(dists,
+                                                                       cap_range = cap_range,
+                                                                       age = 10,
+                                                                       model = 1), simplify = FALSE)
 
 ddd_free_tes_list <- purrr::transpose(ddd_free_tes_list)
 
 eveGNN::export_to_gnn_with_params(ddd_free_tes_list, "tes", undirected = FALSE)
 
-ddd_free_tes_list_test <- eveGNN::get_test_data(ddd_free_tes_list, 0.005)
+ddd_free_tes_list_test <- eveGNN::get_test_data(ddd_free_tes_list, 0.025)
 
 if (!dir.exists("MLE")) {
   dir.create("MLE")
