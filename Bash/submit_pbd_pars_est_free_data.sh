f7404403ff5d4b5f09f9e0d2daeff8627f0e09f1
Tianjian <tianjianqin@outlook.com>
2023-11-27 19:45:20 +0100

    shell script for PBD simulation

---
+++ Bash/submit_pbd_pars_est_free_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=6-23:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=gnn_pbd_pars_free
+#SBATCH --output=logs/gnn_pbd_pars_free-%j.log
+#SBATCH --mem=64GB
+#SBATCH --partition=regular
+
+name=${1}
+
+ml R
+Rscript ../Script/pbd_pars_est_free_data.R ${name}
