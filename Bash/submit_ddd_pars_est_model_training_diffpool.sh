4da5335a73b8f7d39eaefa0e39c1a8c354924518
Tianjian <tianjianqin@outlook.com>
2023-12-04 12:37:24 +0100

    fix bash script for different gnn layer depths

--- Bash/submit_ddd_pars_est_model_training_diffpool.sh
+++ Bash/submit_ddd_pars_est_model_training_diffpool.sh
@@ -12,16 +12,17 @@ ml Python/3.8.16-GCCcore-11.2.0
 source $HOME/venvs/eve/bin/activate
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <name> <task_type>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <name> <task_type> <gnn_depth>"
     exit 1
 fi
 
 # Get the arguments
 name=$1
 task_type=$2
+gnn_depth=$3
 
 # Call the Python script with the arguments
-python ../Script/train_ddd_pars_est_DiffPool.py "$name" "$task_type"
+python ../Script/train_ddd_pars_est_DiffPool.py "$name" "$task_type" "$gnn_depth"
 
 deactivate
