6b631ea23b0f23bfaa4fd4d32b455f317efda317
Tianjian <tianjianqin@outlook.com>
2023-12-27 11:45:09 +0100

    increase to 32 CPUs for DDD MLE

--- Bash/submit_ddd_pars_est_free_data.sh
+++ Bash/submit_ddd_pars_est_free_data.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --job-name=gnn_ddd_pars_free
 #SBATCH --output=logs/gnn_ddd_pars_free-%j.log
-#SBATCH --mem=80GB
+#SBATCH --mem=128GB
 #SBATCH --partition=regular
 
 name=${1}
