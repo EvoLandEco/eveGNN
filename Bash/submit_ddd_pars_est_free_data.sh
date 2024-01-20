344c171803b648c5a5286eea54339cc947e64bea
Tianjian <tianjianqin@outlook.com>
2024-01-20 10:12:04 +0100

    correction of ddd pars free est resource requirement

--- Bash/submit_ddd_pars_est_free_data.sh
+++ Bash/submit_ddd_pars_est_free_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=9-23:59:00
+#SBATCH --time=0-23:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=6
 #SBATCH --job-name=gnn_ddd_pars_free
 #SBATCH --output=logs/gnn_ddd_pars_free-%j.log
 #SBATCH --mem=80GB
