92323bfe746f8fa436d3e945fa42dd45277469ed
Tianjian <tianjianqin@outlook.com>
2024-02-26 12:27:29 +0100

    experimenting undirected trees with a master node connecting all internal nodes

--- R/data_export.R
+++ R/data_export.R
@@ -58,7 +58,7 @@ export_to_gnn_bootstrap <- function(data, meta, index, path, undirected = FALSE)
 
 
 #' @export export_to_gnn_with_params
-export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE) {
+export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE, master = FALSE) {
   path <- file.path("GNN/tree/")
   path_EL <- file.path("GNN/tree/EL/")
   eve:::check_path(path)
@@ -71,7 +71,7 @@ export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE) {
       cap <- data$pars[[i]][3]
       age <- data$age[[i]]
       file_name <- paste0(path, "/tree_", la, "_", mu, "_", cap, "_", age, "_", i, ".rds")
-      saveRDS(tree_to_connectivity(data$tas[[i]], undirected = undirected), file = file_name)
+      saveRDS(tree_to_connectivity(data$tas[[i]], undirected = undirected, master = master), file = file_name)
     }
     for (i in seq_along(data$tas)) {
       la <- data$pars[[i]][1]
@@ -79,7 +79,7 @@ export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE) {
       cap <- data$pars[[i]][3]
       age <- data$age[[i]]
       file_name <- paste0(path_EL, "/EL_", la, "_", mu, "_", cap, "_", age, "_", i, ".rds")
-      saveRDS(tree_to_adj_mat(data$tas[[i]]), file = file_name)
+      saveRDS(tree_to_adj_mat(data$tas[[i]], master = master), file = file_name)
     }
   } else if (which == "tes") {
     for (i in seq_along(data$tes)) {
@@ -88,7 +88,7 @@ export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE) {
       cap <- data$pars[[i]][3]
       age <- data$age[[i]]
       file_name <- paste0(path, "/tree_", la, "_", mu, "_", cap, "_", age, "_", i, ".rds")
-      saveRDS(tree_to_connectivity(data$tes[[i]], undirected = undirected), file = file_name)
+      saveRDS(tree_to_connectivity(data$tes[[i]], undirected = undirected, master = master), file = file_name)
     }
     for (i in seq_along(data$tes)) {
       la <- data$pars[[i]][1]
@@ -96,7 +96,7 @@ export_to_gnn_with_params <- function(data, which = "tas", undirected = FALSE) {
       cap <- data$pars[[i]][3]
       age <- data$age[[i]]
       file_name <- paste0(path_EL, "/EL_", la, "_", mu, "_", cap, "_", age, "_", i, ".rds")
-      saveRDS(tree_to_adj_mat(data$tes[[i]]), file = file_name)
+      saveRDS(tree_to_adj_mat(data$tes[[i]], master = master), file = file_name)
     }
   }
 }
