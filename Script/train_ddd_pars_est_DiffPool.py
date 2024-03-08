6dd1e034d08fe87b5fdb3eb577d37e093aa9a206
Tianjian <tianjianqin@outlook.com>
2024-03-08 12:21:15 +0100

    try another flatten methood after GCN modules

--- Script/train_ddd_pars_est_DiffPool.py
+++ Script/train_ddd_pars_est_DiffPool.py
@@ -452,7 +452,7 @@ def main():
             x, adj, l2, e2 = dense_diff_pool(x, adj, s)
 
             x = self.gnn3_embed(x, adj)
-            x = x.view(x.size(0), -1)
+            x = x.flatten()
             x = self.linear_layers(x)
 
             return x, l1 + l2, e1 + e2
