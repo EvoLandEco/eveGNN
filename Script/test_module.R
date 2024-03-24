4357fe4b6b07cb4c680ebbe5a1d254deaabaf012
Tianjian <tianjianqin@outlook.com>
2024-03-24 16:04:12 +0100

    try reinstall the packages on the computing cluster

--- Script/test_module.R
+++ Script/test_module.R
@@ -1,1 +1,4 @@
-params <- yaml::read_yaml("../Config/ddd_sim.yaml")
+remotes::install_github("YuLab-SMU/ggtree")
+remotes::install_github("thijsjanzen/treestats")
+remotes::install_github("EvoLandEco/eve")
+remotes::install_github("EvoLandEco/eveGNN")
