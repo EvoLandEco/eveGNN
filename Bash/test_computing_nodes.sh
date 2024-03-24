2441e03f15b61a1e44744b366158ecf649216ac8
Tianjian <tianjianqin@outlook.com>
2024-03-24 15:21:08 +0100

    scripts for testing cluster issue

---
+++ Bash/test_computing_nodes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=0-00:09:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=test_module
+#SBATCH --output=logs/test_module_%j.log
+#SBATCH --mem=2GB
+#SBATCH --partition=regular
+
+ml R
+ml GSL
+
+Rscript ../Script/test_module.R
---
+++ Script/test_module.R
@@ -0,0 +1,1 @@
+params <- yaml::read_yaml("../Config/ddd_sim.yaml")
