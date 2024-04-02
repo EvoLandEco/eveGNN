38d794d140be5496fc644ef2c9adab73a9b94695
Tianjian <tianjianqin@outlook.com>
2024-04-02 12:04:43 +0200

    Parallel safe approach to save MLE results

--- Script/ddd_pars_est_free_mle.R
+++ Script/ddd_pars_est_free_mle.R
@@ -30,10 +30,8 @@ tryCatch(
     differences <- eveGNN::all_differences(as.numeric(ml[1:3]), data$pars[[i]])
     differences$nnode <- data$tes[[i]]$Nnode
 
-    # Save the differences to an RDS file with a timestamp-based filename
-    timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-    timestamp <- paste0(timestamp, sample.int(1000, 1))
-    filename <- paste0("differences_", timestamp, ".rds")
+    filename <- paste0("differences_", i, ".rds")
+
     saveRDS(differences, file = filename)
 
     return(differences)
@@ -71,10 +69,7 @@ tryCatch(
     differences <- eveGNN::all_differences(as.numeric(ml[1:3]), data$pars[[i]])
     differences$nnode <- data$tes[[i]]$Nnode
 
-    # Save the differences to an RDS file with a timestamp-based filename
-    timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-    timestamp <- paste0(timestamp, sample.int(1000, 1))
-    filename <- paste0("differences_", timestamp, ".rds")
+    filename <- paste0("differences_", i, ".rds")
     saveRDS(differences, file = filename)
 
     return(differences)
