b4eebb6aa1cc1db7fcaa5554e7bea49c5951dfdb
Tianjian <tianjianqin@outlook.com>
2024-04-18 10:33:49 +0200

    DDD polymorph scripts

---
+++ Bash/submit_ddd_pars_est_free_mle_poly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=9:59:59
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=gnn_ddd_pars_free_mle_poly
+#SBATCH --mem=2GB
+#SBATCH --partition=regular
+
+index=${1}
+name=${2}
+
+ml R
+Rscript ../Script/ddd_pars_est_free_mle_poly.R ${index} ${name}
---
+++ Script/ddd_pars_est_free_mle_poly.R
@@ -0,0 +1,87 @@
+args <- commandArgs(TRUE)
+
+i <- as.numeric(args[1])
+name <- as.character(args[2])
+
+data <- readRDS(file.path(name, "DDD_MLE_TES/MLE_DATA/ddd_mle_poly.rds"))
+
+setwd(name)
+
+if (!dir.exists("DDD_MLE_POLY")) {
+  dir.create("DDD_MLE_POLY")
+}
+
+setwd("DDD_MLE_POLY")
+
+# Maximize the ML, let the MLE run in the best way possible
+tryCatch(
+  R.utils::withTimeout({
+    ml <- DDD::dd_ML(
+      brts = data$brts[[i]],
+      initparsopt = data$pars[[i]],
+      idparsopt = c(1, 2, 3),
+      btorph = 0,
+      soc = 2,
+      cond = 1,
+      ddmodel = 1,
+      num_cycles = Inf,
+      optimmethod = 'simplex'
+    )
+    # If an error occurred, ml will be NA and we return NA right away.
+    if (length(ml) == 1 && is.na(ml)) {
+      return(NA)
+    }
+    # If no error occurred, proceed as before.
+    names(ml) <- NULL
+    differences <- eveGNN::all_differences(as.numeric(ml[1:3]), data$pars[[i]])
+    differences$nnode <- data$tes[[i]]$Nnode
+
+    filename <- paste0("differences_", i, ".rds")
+
+    saveRDS(differences, file = filename)
+
+    return(differences)
+  }, timeout = 20000),  # in seconds
+  TimeoutException = function(ex) {
+    return(NA)  # Return NA or some other indication of timeout
+  }
+)
+
+if (!dir.exists("NO_INIT")) {
+  dir.create("NO_INIT")
+}
+
+setwd("NO_INIT")
+
+# Start the MLE with no true initial parameters, simulate a random or worst-case scenario
+# TODO: Should read from the config file, get the actual distribution of the parameters
+tryCatch(
+  R.utils::withTimeout({
+    ml <- DDD::dd_ML(
+      brts = data$brts[[i]],
+      initparsopt = c(runif(1, 1, 3), runif(1, 0, 1), runif(1, 200, 800)),
+      idparsopt = c(1, 2, 3),
+      btorph = 0,
+      soc = 2,
+      cond = 1,
+      ddmodel = 1,
+      optimmethod = 'simplex'
+    )
+    # If an error occurred, ml will be NA and we return NA right away.
+    if (length(ml) == 1 && is.na(ml)) {
+      return(NA)
+    }
+    # If no error occurred, proceed as before.
+    names(ml) <- NULL
+    differences <- eveGNN::all_differences(as.numeric(ml[1:3]), data$pars[[i]])
+    differences$nnode <- data$tes[[i]]$Nnode
+
+    filename <- paste0("differences_", i, ".rds")
+    saveRDS(differences, file = filename)
+
+    return(differences)
+  }, timeout = 16000),  # in seconds
+  TimeoutException = function(ex) {
+    return(NA)  # Return NA or some other indication of timeout
+  }
+)
