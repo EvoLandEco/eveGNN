8af6549649e08269419ddf656922d5c3f68bfc75
Tianjian <tianjianqin@outlook.com>
2024-06-20 18:10:05 +0200

    Separate final validation scripts

---
+++ Bash/submit_ddd_pars_est_free_data_val.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=0-23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --job-name=gnn_ddd_pars_free_val
+#SBATCH --output=logs/gnn_ddd_pars_free_val-%j.log
+#SBATCH --mem=32GB
+#SBATCH --partition=regular
+
+name=${1}
+
+ml R
+Rscript ../Script/ddd_pars_est_free_data_val.R ${name}
