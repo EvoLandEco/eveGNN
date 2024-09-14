9767bea8fdc156701779dee4eb6aa4338155e3a1
EvoLandEco <tianjianqin@outlook.com>
2024-09-14 19:43:42 +0200

    Update submit_eve_pars_est_free_data.sh

--- Bash/submit_eve_pars_est_free_data.sh
+++ Bash/submit_eve_pars_est_free_data.sh
@@ -11,4 +11,7 @@
 name=${1}
 
 ml R
+Rscript -e 'install.packages("devtools", repos="http://cran.us.r-project.org")'
+Rscript -e 'devtools::install_github("EvoLandEco/eveGNN@multimodal-stacking-boosting")'
+Rscript -e 'devtools::install_github("HHildenbrandt/evesim@tianjian")'
 Rscript ../Script/eve_pars_est_free_data.R ${name}
