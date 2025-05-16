aab8c54db716da8a7a074548c38e94e1640cbf0c
Tianjian <tianjian.qin@wur.nl>
2025-05-16 10:06:45 +0200

    Update bd_pars_est_with_ddd.R

--- Script/bd_pars_est_with_ddd.R
+++ Script/bd_pars_est_with_ddd.R
@@ -73,13 +73,13 @@ args <- commandArgs(TRUE)
 
 name <- as.character(args[1])
 
-params <- yaml::read_yaml("../Config/bd_sim.yaml")
-
-if (!dir.exists(name)) {
-  dir.create(name)
-}
-
-setwd(name)
+# params <- yaml::read_yaml("../Config/bd_sim.yaml")
+#
+# if (!dir.exists(name)) {
+#   dir.create(name)
+# }
+#
+# setwd(name)
 
 # dists <- params$dists
 # within_ranges <- params$within_ranges
