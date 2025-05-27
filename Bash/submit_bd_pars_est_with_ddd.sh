f62cefafaa249e91575063985c309cd3a57b3d90
Tianjian <tianjian.qin@wur.nl>
2025-05-27 12:27:41 +0200

    Lower resource requirement

--- Bash/submit_bd_pars_est_with_ddd.sh
+++ Bash/submit_bd_pars_est_with_ddd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=1-23:59:00
+#SBATCH --time=00:20:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name=gnn_bd_pars_with_ddd
 #SBATCH --output=logs/gnn_bd_pars_with_ddd-%j.log
-#SBATCH --mem=80GB
+#SBATCH --mem=3GB
 #SBATCH --partition=regular
 
 name=${1}
